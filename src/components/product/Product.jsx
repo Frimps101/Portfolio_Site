bbb3ab1feb8748fec5866489035894524e692810
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-04-26 22:38:55 +0000

    Made few changes

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,44 +1,48 @@
 import React from "react";
 import "./product.css";
-import "../../data";
+import { products } from "../../data";
 
 const Product = ({ link, title, desc, tech }) => {
   console.log(title);
   return (
-    <div className="p">
-      <div className="p-icons">
-        <div className="p-icons--wrapper">
-          <a
-            rel="noreferrer"
-            className="p-icons--link"
-            target="_blank"
-            href="https://github.com/Frimps101"
-          >
-            <i class="fa-brands fa-github icon"></i>
-          </a>
-        </div>
-        <div className="p-icons--wrapper">
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://github.com/Frimps101"
-          >
-            <i class="fa-solid fa-arrow-up-right-from-square icon"></i>
-          </a>
-        </div>
-      </div>
+    <>
+      {products.map((item) => (
+        <div className="p">
+          <div className="p-icons">
+            <div className="p-icons--wrapper">
+              <a
+                rel="noreferrer"
+                className="p-icons--link"
+                target="_blank"
+                href={link}
+              >
+                <i class="fa-brands fa-github icon"></i>
+              </a>
+            </div>
+            <div className="p-icons--wrapper">
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/Frimps101"
+              >
+                <i class="fa-solid fa-arrow-up-right-from-square icon"></i>
+              </a>
+            </div>
+          </div>
 
-      <div className="p-text">
-        <h3 className="p-title">{title}</h3>
-        <p className="p-text__content">{desc}</p>
-      </div>
+          <div className="p-text">
+            <h3 className="p-title">{title}</h3>
+            <p className="p-text__content">{desc}</p>
+          </div>
 
-      <div className="p-tech">
-        {tech?.map((item) => (
-          <small key={item}>{item}</small>
-        ))}
-      </div>
-    </div>
+          <div className="p-tech">
+            {tech?.map((item) => (
+              <small key={item}>{item}</small>
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
